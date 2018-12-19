08a56887122122400e23e6a9e55e588eddea4455
Cynthia19990222 <zxcccc987@qq.com>
2018-12-19 09:18:26 +0800

    Initial Commit

---
+++ try-4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  try-4
+//
+//  Created by s20171103197 on 2018/12/19.
+//  Copyright © 2018 s20171103197. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
